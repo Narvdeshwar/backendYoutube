fd34858161c1cc1e029f99fb4e206066975b8be1
narvdeshwar <sam.07soni@gmail.com>
2024-09-01 20:18:08 +0530

    apirespose and apierror code has been added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 // require('dotenv').config({path:'./env'})
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
-import dotenv  from 'dotenv';
+import dotenv from 'dotenv';
 
 dotenv.config({ path: './env' })
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => console.log(`Server is running!!!`)
+    )
+  })
+  .catch((error) => {
+    console.log("Database connection failed !!!", error);
+  });
 
 
 
