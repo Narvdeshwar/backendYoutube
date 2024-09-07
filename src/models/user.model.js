91a9da5599ad423421098b1fcfa472f06c42baa3
narvdeshwar <sam.07soni@gmail.com>
2024-09-07 07:36:20 +0530

    add jwt bcrypt and some method related to userschema when the password is get encrypted

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,8 +1,9 @@
 import mongoose, { Schema } from "mongoose";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const userSchema = Schema(
   {
-    username: {
+    userName: {
       type: String,
       unique: true,
       required: true,
@@ -17,7 +18,7 @@ const userSchema = Schema(
       lowercase: true,
       trim: true,
     },
-    fullname: {
+    fullName: {
       type: String,
       required: true,
       trim: true,
@@ -48,5 +49,43 @@ const userSchema = Schema(
     timestamps: true,
   },
 );
+
+// password save krne se phle below line execute krna hai ki khi password change hua hai ki nhi age change hua hai to encrypt kr do otherwise mt kro aur yha hm arrow function ka use isliye nhi kr rhe hai kyuki uske pass userSchema ke fields ka refernce nhi rhega .
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+// custom method => isPasswordCorrect method either true or false value return karega ki authenetication ke time
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+//jwt ek bearer token hai mtlb jiske pass bhi "token" hoga ye use data send kr dega
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      userName: this.userName,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    (expireIn = process.env.ACCESS_TOKEN_EXPIRY),
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    (expireIn = process.env.ACCESS_TOKEN_EXPIRY),
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
