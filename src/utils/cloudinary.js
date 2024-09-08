87f5d16639604f9be662c213b4a7e9c7cd07fb78
narvdeshwar <sam.07soni@gmail.com>
2024-09-08 09:20:41 +0530

    completed multer and cloudinary phase

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const cloudinaryUpload = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      console.log("file path is not found");
+      return null;
+    }
+    // upload the file over the cloudinary
+    const response = await cloudinary.v2.uploader.upload(localFilePath, {
+      resource_type: auto, // checking the resource type
+    });
+    console.log("file upload on cloudinary successfully", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the temporary saved file as the file upload operation get failed
+    return null;
+  }
+};
+
+export { cloudinaryUpload };
