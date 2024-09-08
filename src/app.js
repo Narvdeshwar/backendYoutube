314b597de038b7ae0cdc22e690914fbd5b074bd6
narvdeshwar <sam.07soni@gmail.com>
2024-09-08 17:31:27 +0530

    userregister routes completed

--- src/app.js
+++ src/app.js
@@ -6,29 +6,42 @@ const app = express();
 
 // Middleware to enable Cross-Origin Resource Sharing (CORS)
 // Allows the server to accept requests from specified origins
-app.use(cors({
-  origin: process.env.CORS_ORIGIN, // The origin URL(s) allowed to access the server
-  credentials: true // Allows cookies to be sent with requests
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN, // The origin URL(s) allowed to access the server
+    credentials: true, // Allows cookies to be sent with requests
+  }),
+);
 
 // Middleware to parse incoming JSON payloads
 // Limits the size of the JSON payload to 16KB
-app.use(express.json({
-  limit: "16kb"
-}))
+app.use(
+  express.json({
+    limit: "16kb",
+  }),
+);
 
 // Middleware to parse URL-encoded data from forms or query strings
 // extended: true allows for rich objects and arrays to be encoded into the URL-encoded format
-app.use(express.urlencoded({
-  extended: true,
-  limit: "16kb"
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  }),
+);
 
 // Middleware to serve static files (e.g., images, CSS, JavaScript,pdf) from the 'public' directory
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 // Middleware to parse and manage cookies
 // Useful for reading and writing cookies during CRUD operations
-app.use(cookieParser())
+app.use(cookieParser());
+
+// import routes for register the user
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter);
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 // require('dotenv').config({path:'./env'})
 import { app } from "./app.js";
 import connectDB from "./db/index.js";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config({ path: './env' })
+dotenv.config({ path: "./env" });
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => console.log(`Server is running!!!`)
-    )
+    app.listen(process.env.PORT || 8000, () =>
+      console.log(`Server is running on port http://localhost:${process.env.PORT}`),
+    );
   })
   .catch((error) => {
     console.log("Database connection failed !!!", error);
   });
-
-
-
 
 /**IFEE method to establish the database connection */
 
